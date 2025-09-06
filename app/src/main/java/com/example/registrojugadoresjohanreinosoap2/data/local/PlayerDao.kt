207e41d9ad3966634f65e2154cc36b39f0a1fb1e
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-05 20:28:55 -0600

    PlayerDao terminado

---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/local/PlayerDao.kt
@@ -0,0 +1,26 @@
+package com.example.registrojugadoresjohanreinosoap2.data.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlayerDao {
+
+    @Query("SELECT*FROM jugadores ORDER BY Jugadorid DESC")
+        fun observerAll(): Flow<List<PlayerEntity>>
+
+    @Query("SELECT*FROM jugadores WHERE Jugadorid=:id")
+        suspend fun getById(id:Int): PlayerEntity?
+
+    @Upsert
+    suspend fun upsert(player: PlayerEntity):Long
+
+    @Delete
+    suspend fun delete(entity: PlayerEntity)
+
+    @Query("DELETE FROM jugadores WHERE Jugadorid=:id")
+    suspend fun deleteById(id:Int)
+}
