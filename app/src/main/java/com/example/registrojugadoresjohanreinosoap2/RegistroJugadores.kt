8e7de7a73e537e5b12964a20c57823a0828184d3
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-05 20:33:51 -0600

    RegistroJugadores Hilt terminada

---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/RegistroJugadores.kt
@@ -0,0 +1,8 @@
+package com.example.registrojugadoresjohanreinosoap2
+
+import androidx.navigation.NavHostController
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroJugadores(navHost: NavHostController) : android.app.Application() {
+}
