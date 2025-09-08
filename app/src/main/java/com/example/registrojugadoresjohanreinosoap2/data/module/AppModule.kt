a8d5a48fb7a767ff41b51107206463ed55959ab0
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-08 13:09:25 -0600

    AppModule terminado

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/module/AppModule.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/module/AppModule.kt
@@ -3,13 +3,20 @@ package com.example.registrojugadoresjohanreinosoap2.data.module
 import android.content.Context
 import androidx.room.Room
 import com.example.registrojugadoresjohanreinosoap2.data.db.PlayerDb
+import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerDao
+import com.example.registrojugadoresjohanreinosoap2.data.repository.PlayerRepositoryImpl
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.PlayerRepository
+import com.example.registrojugadoresjohanreinosoap2.domain.usecase.DeletePlayerUseCase
+import com.example.registrojugadoresjohanreinosoap2.domain.usecase.GetPlayerUseCase
+import com.example.registrojugadoresjohanreinosoap2.domain.usecase.ObservePlayersUseCase
+import com.example.registrojugadoresjohanreinosoap2.domain.usecase.UpsertPlayerUseCase
+import com.example.registrojugadoresjohanreinosoap2.domain.usecase.ValidationPlayerUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
 
 @InstallIn(
     SingletonComponent::class)
@@ -18,11 +25,52 @@ import javax.inject.Singleton
 object AppModule {
     @Provides
     @Singleton
-    fun providePlayer(@ApplicationContext appContext: Context)=
-        Room.databaseBuilder(
-                appContext,
-                PlayerDb::class.java,
-                "player_db"
-            ).fallbackToDestructiveMigration(false)
+    fun providePlayerDB(@ApplicationContext appContext: Context): PlayerDb {
+        return Room.databaseBuilder(
+            appContext,
+            PlayerDb::class.java,
+            "PlayerDb"
+        ).fallbackToDestructiveMigration(false)
             .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePlayerDao(playerDb: PlayerDb): PlayerDao {
+        return playerDb.playerDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providePlayerRepositoryImpl(playerDao: PlayerDao): PlayerRepositoryImpl {
+        return PlayerRepositoryImpl(playerDao)
+    }
+
+    @Provides
+    @Singleton
+    fun providePlayerRepository(impl: PlayerRepositoryImpl): PlayerRepository {
+        return impl
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideGetPlayerUseCase(repo: PlayerRepository) = GetPlayerUseCase(repo)
+
+    @Provides
+    @Singleton
+    fun provideUpsertPlayerUseCase(repo: PlayerRepository) = UpsertPlayerUseCase(repo)
+
+    @Provides
+    @Singleton
+    fun provideDeletePlayerUseCase(repo: PlayerRepository) = DeletePlayerUseCase(repo)
+
+    @Provides
+    @Singleton
+    fun provideObservePlayersUseCase(repo: PlayerRepository) = ObservePlayersUseCase(repo)
+
+    @Provides
+    @Singleton
+    fun provideValidationPlayerUseCase(repo: PlayerRepository) = ValidationPlayerUseCase(repo)
+
 }
