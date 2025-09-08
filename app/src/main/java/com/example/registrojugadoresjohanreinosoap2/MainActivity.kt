ed7e2eb8df081067ccf74b098eef5807d3ef49c1
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-08 13:10:41 -0600

    Main terminado

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/MainActivity.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.registrojugadoresjohanreinosoap2.presentation.edit.EditPlayerScreen
+import com.example.registrojugadoresjohanreinosoap2.presentation.list.PlayerListScreen
 import com.example.registrojugadoresjohanreinosoap2.ui.theme.RegistroJugadoresJohanReinosoAp2Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +20,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             RegistroJugadoresJohanReinosoAp2Theme {
 
-            val navHost= rememberNavController()
-                RegistroJugadores(navHost)
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "playerList"
+                ) {
+                    composable("playerList") {
+                        PlayerListScreen(
+                            onNavigateToEdit = { id ->
+                                navController.navigate("editPlayer/$id")
+                            },
+                            onNavigateToCreate = {
+                                navController.navigate("editPlayer/0")
+                            }
+                        )
+                }
+                    composable("editPlayer/{id}") { backStackEntry ->
+                        val id = backStackEntry.arguments?.getString("id")?.toIntOrNull()
+                        EditPlayerScreen(playerId = id, navController = navController)
             }
         }
+    }
+}
     }
 }
