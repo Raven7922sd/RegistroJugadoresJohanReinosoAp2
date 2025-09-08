3f6ce99651aa4224498171f1694eb3adaab00c39
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-08 14:13:41 -0600

    Implementaciones en PlayerRepositoryImpl

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/repository/PlayerRepositoryImpl.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/repository/PlayerRepositoryImpl.kt
@@ -1,31 +1,38 @@
 package com.example.registrojugadoresjohanreinosoap2.data.repository
 
-import androidx.room.Delete
 import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerDao
+import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerEntity
 import com.example.registrojugadoresjohanreinosoap2.data.mapper.toDomain
 import com.example.registrojugadoresjohanreinosoap2.data.mapper.toEntity
 import com.example.registrojugadoresjohanreinosoap2.domain.model.Player
 import com.example.registrojugadoresjohanreinosoap2.domain.repository.PlayerRepository
+import javax.inject.Inject
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.Flow
-import java.util.Collections.list
 
-class PlayerRepositoryImpl(
-    private val dao: PlayerDao
-): PlayerRepository {
+class PlayerRepositoryImpl @Inject constructor(
+    private val playerDao: PlayerDao
+) : PlayerRepository {
 
-    override fun observePlayer(): Flow<List<Player>> =
-        dao.observerAll().map { list ->
-            list.map { it.toDomain() } }
-
-    override suspend fun getPlayer(id: Int): Player?=dao.getById(id)?.toDomain()
-
+    override fun observePlayer(): Flow<List<Player>> {
+        return playerDao.observerAll().map { entities ->
+            entities.map { it.toDomain() }
+        }
+    }
+    override suspend fun getPlayer(id: Int): Player? {
+        return playerDao.getById(id)?.toDomain()
+    }
     override suspend fun upsert(player: Player): Int {
-        dao.upsert(player.toEntity())
-        return player.Jugadorid
+        val entity = player.toEntity()
+        val result = playerDao.upsert(entity)
+        return if (player.Jugadorid == 0) result.toInt() else player.Jugadorid
     }
 
-    override suspend fun delete(id: Int){
-        dao.deleteById(id)
+    override suspend fun delete(id: Int) {
+        playerDao.deleteById(id)
+    }
+
+    override suspend fun getAllPlayers(): List<Player> {
+        return playerDao.getAllPlayers().map { it.toDomain() }
     }
 }
