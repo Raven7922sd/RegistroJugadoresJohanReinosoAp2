632a6fe4a3f1271cfddd358ea2136bf41155be34
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-09-24 16:28:13 -0600

    PlayerDb actualizado con logros

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/db/PlayerDb.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/db/PlayerDb.kt
@@ -4,15 +4,19 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.registrojugadoresjohanreinosoap2.data.local.GameDao
 import com.example.registrojugadoresjohanreinosoap2.data.local.GameEntity
+import com.example.registrojugadoresjohanreinosoap2.data.local.Logros.LogroDao
+import com.example.registrojugadoresjohanreinosoap2.data.local.Logros.LogroEntity
 import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerDao
 import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerEntity
 
-@Database(entities = [PlayerEntity::class, GameEntity::class],
-    version = 2,
+@Database(entities = [PlayerEntity::class, GameEntity::class, LogroEntity::class],
+    version = 4,
     exportSchema = false)
 
 abstract class PlayerDb: RoomDatabase() {
 
     abstract fun playerDao(): PlayerDao
     abstract fun GameDao(): GameDao
+
+    abstract fun LogroDao(): LogroDao
 }
