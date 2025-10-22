9771c0d8b52d25c96c543409b58d38c01dd9fb68
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-22 12:07:35 -0600

    AppModule actualizado para consumir la api

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/module/AppModule.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/module/AppModule.kt
@@ -6,9 +6,19 @@ import com.example.registrojugadoresjohanreinosoap2.data.db.PlayerDb
 import com.example.registrojugadoresjohanreinosoap2.data.local.GameDao
 import com.example.registrojugadoresjohanreinosoap2.data.local.Logros.LogroDao
 import com.example.registrojugadoresjohanreinosoap2.data.local.PlayerDao
+import com.example.registrojugadoresjohanreinosoap2.data.remoteApi.JugadorApi
+import com.example.registrojugadoresjohanreinosoap2.data.remoteApi.MovimientosApi
+import com.example.registrojugadoresjohanreinosoap2.data.remoteApi.PartidaApi
 import com.example.registrojugadoresjohanreinosoap2.data.repository.GameRepositoryImpl
 import com.example.registrojugadoresjohanreinosoap2.data.repository.PlayerRepositoryImpl
+import com.example.registrojugadoresjohanreinosoap2.data.repository.apiRepository.ApiJugadoresRepositoryImpl
+import com.example.registrojugadoresjohanreinosoap2.data.repository.apiRepository.ApiMovimientoRepositoryImpl
+import com.example.registrojugadoresjohanreinosoap2.data.repository.apiRepository.ApiPartidaRepositoryImpl
 import com.example.registrojugadoresjohanreinosoap2.data.repository.logroRepository.LogroRepositoryImpl
+import com.example.registrojugadoresjohanreinosoap2.domain.model.Movimiento.Movimientos
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.ApiRepository.JugadorApiRepository
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.ApiRepository.MovimientoApiRepository
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.ApiRepository.PartidaApiRepository
 import com.example.registrojugadoresjohanreinosoap2.domain.repository.GameRepository
 import com.example.registrojugadoresjohanreinosoap2.domain.repository.LogrosRepository.LogroRepository
 import com.example.registrojugadoresjohanreinosoap2.domain.repository.PlayerRepository
@@ -22,11 +32,15 @@ import com.example.registrojugadoresjohanreinosoap2.domain.usecase.gameUseCase.G
 import com.example.registrojugadoresjohanreinosoap2.domain.usecase.gameUseCase.ObserveGameUseCase
 import com.example.registrojugadoresjohanreinosoap2.domain.usecase.gameUseCase.UpsertGameCase
 import com.example.registrojugadoresjohanreinosoap2.domain.usecase.gameUseCase.ValidationGameUseCase
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @InstallIn(
@@ -34,6 +48,65 @@ import javax.inject.Singleton
 @Module
 
 object AppModule {
+    private const val BASE_URL = "https://gestionhuacalesapi.azurewebsites.net/"
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+    @Provides
+    @Singleton
+    fun providePartidaApi(moshi: Moshi): PartidaApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(PartidaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovimientoApi(moshi: Moshi): MovimientosApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(MovimientosApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideJugadorApi(moshi: Moshi): JugadorApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(JugadorApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providePartidaApiRepository(api: PartidaApi): PartidaApiRepository {
+        return ApiPartidaRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideJugadorApiRepository(api: JugadorApi): JugadorApiRepository {
+        return ApiJugadoresRepositoryImpl(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovimientoApiRepository(api: MovimientosApi): MovimientoApiRepository {
+        return ApiMovimientoRepositoryImpl(api)
+    }
+
+
+
     @Provides
     @Singleton
     fun providePlayerDB(@ApplicationContext appContext: Context): PlayerDb {
