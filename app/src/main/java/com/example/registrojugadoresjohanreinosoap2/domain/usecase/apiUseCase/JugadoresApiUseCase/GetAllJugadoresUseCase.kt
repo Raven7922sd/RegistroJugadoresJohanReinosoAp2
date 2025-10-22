9c47ea6613914adb947337d7353f52d485fdf956
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-22 12:01:10 -0600

    getjugadoresUseCase creado

---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/domain/usecase/apiUseCase/JugadoresApiUseCase/GetAllJugadoresUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.registrojugadoresjohanreinosoap2.domain.usecase.apiUseCase.JugadoresApiUseCase
+
+import com.example.registrojugadoresjohanreinosoap2.domain.model.Player
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.ApiRepository.JugadorApiRepository
+import javax.inject.Inject
+
+class GetAllJugadoresUseCase @Inject constructor(
+    private val repository: JugadorApiRepository
+) {
+    suspend operator fun invoke(): List<Player> {
+        return repository.getAllJugadores()
+    }
+}
---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/domain/usecase/apiUseCase/JugadoresApiUseCase/GetJugadoresUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.registrojugadoresjohanreinosoap2.domain.usecase.apiUseCase.JugadoresApiUseCase
+
+
+import com.example.registrojugadoresjohanreinosoap2.domain.model.Player
+import com.example.registrojugadoresjohanreinosoap2.domain.repository.ApiRepository.JugadorApiRepository
+import javax.inject.Inject
+
+class GetJugadoresUseCase @Inject constructor(
+    private val repository: JugadorApiRepository
+) {
+    suspend operator fun invoke(): List<Player> {
+        return repository.getAllJugadores()
+    }
+}
