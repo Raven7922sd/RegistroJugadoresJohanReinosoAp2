6c7387e1daf2f9df7c00fcfebb08ccb14b2e4a76
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-22 11:49:38 -0600

    JugadotDto Creado

---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/remoteApi/dto/JugadorDto.kt
@@ -0,0 +1,7 @@
+package com.example.registrojugadoresjohanreinosoap2.data.remoteApi.dto
+
+data class JugadorDto(
+    val jugadorId: Int? = null,
+    val nombres: String,
+    val email: String
+)
