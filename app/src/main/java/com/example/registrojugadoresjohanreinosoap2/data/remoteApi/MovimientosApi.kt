b17facc9f18be42b9c65786fe588c41f08812bf1
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-22 11:54:34 -0600

    MovimientoApi creado

---
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/remoteApi/MovimientosApi.kt
@@ -0,0 +1,15 @@
+package com.example.registrojugadoresjohanreinosoap2.data.remoteApi;
+
+import com.example.registrojugadoresjohanreinosoap2.data.remoteApi.dto.MovimientoDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface MovimientosApi {
+    @GET("api/Movimientos/{partidaId}")
+    suspend fun getMovimientos(@Path("partidaId") partidaId: Int): List<MovimientoDto>
+
+    @POST("api/Movimientos")
+    suspend fun crearMovimiento(@Body movimiento: MovimientoDto)
+}
