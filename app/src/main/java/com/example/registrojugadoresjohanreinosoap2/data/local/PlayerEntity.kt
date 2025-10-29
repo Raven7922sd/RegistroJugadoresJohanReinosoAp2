0ff9ed2242b8698e413710c44013ffb8b04ba52e
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-29 17:48:51 -0600

    PlayerEntity modificado

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/local/PlayerEntity.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/local/PlayerEntity.kt
@@ -2,11 +2,15 @@ package com.example.registrojugadoresjohanreinosoap2.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "Jugadores")
-class PlayerEntity (
-    @PrimaryKey(autoGenerate = true )
-    val Jugadorid: Int=0,
+data class PlayerEntity (
+    @PrimaryKey
+    val Jugadorid: String = UUID.randomUUID().toString(),
+    val remoteId: Int? = null,
     val Nombres: String,
-    val Partidas: Int)
+    val Partidas: Int,
+    val isPendingCreate: Boolean = false
+)
 {}
