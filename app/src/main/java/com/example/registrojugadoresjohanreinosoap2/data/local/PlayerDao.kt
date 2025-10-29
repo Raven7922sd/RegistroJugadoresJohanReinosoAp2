b81d1416425e0f9cc6beb57d3325cd0037106862
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-29 17:48:29 -0600

    getPlayerByName agregado en PlayerDao

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/local/PlayerDao.kt
+++ app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/local/PlayerDao.kt
@@ -13,7 +13,7 @@ interface PlayerDao {
         fun observerAll(): Flow<List<PlayerEntity>>
 
     @Query("SELECT*FROM jugadores WHERE Jugadorid=:id")
-        suspend fun getById(id:Int): PlayerEntity?
+        suspend fun getById(id: String): PlayerEntity?
 
     @Query("SELECT*FROM jugadores WHERE LOWER(Nombres) = LOWER(:nombre)")
     suspend fun getByName(nombre: String): List<PlayerEntity>
@@ -21,12 +21,23 @@ interface PlayerDao {
     @Upsert
     suspend fun upsert(player: PlayerEntity):Long
 
+    @Query("SELECT * FROM Jugadores WHERE Nombres = :nombre")
+    suspend fun getPlayersByName(nombre: String): List<PlayerEntity>
+
     @Delete
     suspend fun delete(entity: PlayerEntity)
 
     @Query("DELETE FROM jugadores WHERE Jugadorid=:id")
-    suspend fun deleteById(id:Int)
+    suspend fun deleteById(id: String)
 
     @Query("SELECT * FROM jugadores")
     suspend fun getAllPlayers(): List<PlayerEntity>
+
+
+    @Query("SELECT * FROM Jugadores WHERE isPendingCreate = 1")
+    suspend fun getPendingCreateJugadores(): List<PlayerEntity>
+
+
+    @Query("SELECT * FROM Jugadores WHERE remoteId = :remoteId")
+    suspend fun getByRemoteId(remoteId: Int): PlayerEntity?
 }
