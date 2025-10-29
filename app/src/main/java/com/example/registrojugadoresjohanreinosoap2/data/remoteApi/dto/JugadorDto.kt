7456ea2212ea20aba02b196234520abbae706e09
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-10-29 17:47:20 -0600

    JugadoreResponse y JugadorRequest creados

--- app/src/main/java/com/example/registrojugadoresjohanreinosoap2/data/remoteApi/dto/JugadorDto.kt
+++
@@ -1,7 +0,0 @@
-package com.example.registrojugadoresjohanreinosoap2.data.remoteApi.dto
-
-data class JugadorDto(
-    val jugadorId: Int? = null,
-    val nombres: String,
-    val email: String
-)
